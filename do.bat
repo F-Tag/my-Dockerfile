3e40094b5bfafe3908524b77abdb9f4c89448745
Fumiaki Taguchi <25476669+F-Tag@users.noreply.github.com>
2022-06-27 00:02:45 +0900

    fix

--- do.bat
+++ do.bat
@@ -2,4 +2,4 @@ REM build
 docker build -t cuda102 .
 
 REM run with GPUs
-docker run -d --name cuda102 --gpus all -it cuda102 /bin/bash
+docker run -d --name cuda102 --gpus all -it ftag77/cuda102 /bin/bash
