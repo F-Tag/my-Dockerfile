f4725804e85b835ce2d4445a2c3712fbd308652e
Fumiaki Taguchi <25476669+F-Tag@users.noreply.github.com>
2022-06-27 22:07:57 +0900

    add memory option

--- do.bat
+++ do.bat
@@ -2,4 +2,4 @@ REM build
 docker build -t ftag77/cuda102 .
 
 REM run with GPUs
-docker run -d --name cuda102 --gpus all --shm-size=2g -it ftag77/cuda102 /bin/bash
+docker run -d --name cuda102 --gpus all --shm-size=2g --memory=16gb -it ftag77/cuda102 /bin/bash
